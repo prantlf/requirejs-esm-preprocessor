0f4e5bea78d3c284275e65f5d3f8b5e8d7ddd95d
Ferdinand Prantl <prantlf@gmail.com>
2022-04-09 17:11:30 +0200

    docs: Use only requirejs.min, correct text on the pages

--- demo-extern/server.js
+++ demo-extern/server.js
@@ -1,8 +1,8 @@
 const { serve } = require('..')
 serve({
-  isScript: path => path.endsWith('.js') &&
+  isScript: path => path.endsWith('.js') && !path.endsWith('.min.js') &&
     (path.startsWith('/demo-extern/src/') || path.startsWith('/node_modules/')) &&
-    !(path.endsWith('/require.js') || path.endsWith('/require.min.js')),
+    !(path.endsWith('/require.js')),
   dirMap: {
     '/node_modules/': '',
     '/demo-extern/src/': 'src/'
