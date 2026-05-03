880de430a7c7f15408127a19184d89e8ecc4f375
Ferdinand Prantl <prantlf@gmail.com>
2026-05-03 18:52:57 +0200

    feat: Upgrade dependencies

--- bin/requirejs-esm-serve.js
+++ bin/requirejs-esm-serve.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { configure, serve } = require('..')
+const { configure, serve } = require('../dist/cjs')
 const { version } = require('../package.json')
 
 serve(configure(process.argv.slice(2), 'requirejs-esm-serve', version))
